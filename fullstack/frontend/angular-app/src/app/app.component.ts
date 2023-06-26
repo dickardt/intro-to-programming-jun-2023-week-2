d554a19cfd42a08d25f3ec8fa8788e7d955a65d9
Tyler Dickard <Tyler_A_Dickard@progressive.com>
2023-06-26 15:03:35 -0400

    add app

--- fullstack/frontend/angular-app/src/app/app.component.ts
+++ fullstack/frontend/angular-app/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BankAccountComponent } from './components/bank-account.compnent';
+import { HeadingComponent } from './components/heading/heading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [CommonModule, RouterOutlet, BankAccountComponent, HeadingComponent],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+
 })
 export class AppComponent {
-  title = 'angular-app';
+
 }
+
+

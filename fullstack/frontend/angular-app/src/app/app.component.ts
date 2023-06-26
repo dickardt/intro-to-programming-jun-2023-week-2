0e3cdc9c20abcc360f4472e36fb38a9a7d1bb4f1
Tyler Dickard <Tyler_A_Dickard@progressive.com>
2023-06-26 15:19:10 -0400

    last break of the day :)

--- fullstack/frontend/angular-app/src/app/app.component.ts
+++ fullstack/frontend/angular-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BankAccountComponent } from './components/bank-account.compnent';
 import { HeadingComponent } from './components/heading/heading.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BankAccountComponent, HeadingComponent],
+  imports: [CommonModule, RouterOutlet, BankAccountComponent, HeadingComponent, NavigationComponent],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 

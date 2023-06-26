3ad10bbcca4e6839535a5309f5f3e42e43b1a99d
Tyler Dickard <Tyler_A_Dickard@progressive.com>
2023-06-26 16:16:30 -0400

    add checks

--- fullstack/frontend/angular-app/src/app/components/bank-account.compnent.ts
+++ fullstack/frontend/angular-app/src/app/components/bank-account.compnent.ts
@@ -1,36 +1,55 @@
-import { CurrencyPipe } from "@angular/common";
-import { Component, signal } from "@angular/core";
+import { CurrencyPipe, NgIf } from "@angular/common";
+import { Component, Signal, signal } from "@angular/core";
+import { BankAccount } from "../services/bank-account.service";
 
 
 
 @Component({
     selector: 'app-account',
     template: `
-    <section>
-        <span>Your balance is {{balance() | currency}}</span>
-    </section>
-    <section>
-        <label>Amount: <input type="number" #amount /></label>
-    </section>
-    <section>
-        <button class="btn btn-primary" (click)="deposit(amount)">Deposit</button>
-        <button class="btn btn-primary" (click)="withdraw(amount)">Withdraw</button>
-    </section>
-    
+<section>
+<span>Your balance is {{balance() | currency}}</span>
+</section>
+<section>
+<label>Amount: <input type="number" #amount (keyup)="checkForOverdraft(amount.valueAsNumber)"/></label>
+</section>
+<section>
+<div *ngIf="overdraftWarning() === true" class="alert alert-warning">
+    <p>That would overdraft your account!</p>
+</div>
+<button class="btn btn-primary" (click)="deposit(amount)">Deposit</button>
+<button [disabled]="overdraftWarning() === true" (click)="withdraw(amount)">Withdraw</button>
+</section>
+
     `,
     imports: [CurrencyPipe],
     standalone: true
 })
-
 export class BankAccountComponent {
 
-    balance = signal(5000);
 
 
 
+
+    balance: Signal<number>;
+    overdraftWarning = signal(false);
+
+
+    constructor(private account: BankAccount) {
+        this.account = account;
+        this.balance = account.getBalance();
+    }
+
+    checkForOverdraft(amount: number) {
+        if (amount > this.balance()) {
+            this.overdraftWarning.set(true);
+        } else {
+            this.overdraftWarning.set(false);
+        }
+    }
 
     deposit(amount: HTMLInputElement) {
-        this.balance.set(this.balance() + amount.valueAsNumber);
+        this.account.makeDeposit(amount.valueAsNumber);
         amount.value = '';
         amount.focus();
     }
@@ -38,7 +57,7 @@ export class BankAccountComponent {
 
 
     withdraw(amount: HTMLInputElement) {
-        this.balance.set(this.balance() - amount.valueAsNumber);
+        this.account.makeWithdrawal(amount.valueAsNumber);
         amount.value = '';
         amount.focus();
     }

76ffec0c1efe4ebe4a74b38f8a68e4021d285f41
Tyler Dickard <Tyler_A_Dickard@progressive.com>
2023-06-27 15:40:27 -0400

    implement list

--- fullstack/frontend/angular-app/src/app/features/shopping-list/shopping-list.component.ts
+++ fullstack/frontend/angular-app/src/app/features/shopping-list/shopping-list.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { ListComponent } from './components/list/list.component';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    ListComponent,
+    CreateComponent,
+  ],
 })
 export class ShoppingListComponent {
-  constructor() {
-    const myName = 'Tyler';
-    console.log(myName);
-  }
+  shoppingList = signal([
+    { id: '1', description: 'Shampoo', purchased: false },
+    { id: '2', description: 'Lettuce', purchased: true },
+  ]);
 }

da1e1c26ee5fec83194b81699c3871b7963f7355
Tyler Dickard <Tyler_A_Dickard@progressive.com>
2023-06-27 12:26:44 -0400

    basic list

--- fullstack/frontend/angular-app/src/app/features/shopping-list/shopping-list.component.ts
+++ fullstack/frontend/angular-app/src/app/features/shopping-list/shopping-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
